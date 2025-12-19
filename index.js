9d16f8caee97209cdd193facfde3deae1a1bbe4c
anish-X <anish.stha527@gmail.com>
2025-12-19 12:39:09 +0545

    test

---
+++ index.js
@@ -0,0 +1,12 @@
+// index.js
+
+// Default export an object containing the `add` function
+export default {
+  add: (a, b) => {
+    // Ensure both inputs are numbers
+    if (typeof a !== "number" || typeof b !== "number") {
+      throw new Error("Both arguments must be numbers");
+    }
+    return a + b;
+  },
+};
